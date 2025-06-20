a50e48aa793279248990291a772f0cbd4643633e
Cody-Howell <codyhowell44@gmail.com>
2025-06-19 23:42:07 -0600

    Added good XML comments for extension

--- AccountAuthenticator/IdentityMiddlewareExtension.cs
+++ AccountAuthenticator/IdentityMiddlewareExtension.cs
@@ -7,11 +7,26 @@ using System.Threading.Tasks;
 
 namespace AccountAuthenticator;
 
+/// <summary>
+/// Unused XML comment
+/// </summary>
 public static class IdentityMiddlewareExtension {
+    /// <summary>
+    /// Enables the Identity Middleware for AccountAuthenticator. Create a lambda to configure options
+    /// previously held in IIDMiddlewareConfig, with an example below.
+    /// <code>
+    /// app.UseAccountIdentityMiddleware(options => {
+    ///   options.Paths = ["/users", "/user", "/user/signin"];
+    ///   options.Whitelist = "/data";
+    ///   options.ExpirationDate = new TimeSpan(30, 0, 0, 0);
+    ///   options.ReValidationDate = new TimeSpan(5, 0, 0, 0);
+    /// });
+    /// </code>
+    /// </summary>
     public static IApplicationBuilder UseAccountIdentityMiddleware(this IApplicationBuilder app, Action<IDMiddlewareConfig>? configureOptions = null) {
-        var options = new IDMiddlewareConfig();
-        configureOptions?.Invoke(options);
+    var options = new IDMiddlewareConfig();
+    configureOptions?.Invoke(options);
 
-        return app.UseMiddleware<IdentityMiddleware>(options);
-    }
+    return app.UseMiddleware<IdentityMiddleware>(options);
+}
 }
